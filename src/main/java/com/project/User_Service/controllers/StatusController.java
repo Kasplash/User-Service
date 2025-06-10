424f3f98c2f291a26ee2fc1de44577ee5d2800b3
Kasplash <andreas.karlsson03@gmail.com>
2025-06-10 23:36:39 +0200

    status endpoint

---
+++ src/main/java/com/project/User_Service/controllers/StatusController.java
@@ -0,0 +1,13 @@
+package com.project.User_Service.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StatusController {
+
+    @GetMapping("/status")
+    public String status(){
+        return "online";
+    }
+}
